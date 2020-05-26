29c84efb45987c9ee331de5391213227d9849796
msivatg <sivameena913@gmail.com>
2020-05-26 22:55:39 +0530

    upgradation with post and Get methods on Tornado

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 import tornado.web
 import tornado.ioloop
 import asyncio
-
+import json
 
 class basicRequestHandler(tornado.web.RequestHandler):
     def get(self):
@@ -18,17 +18,39 @@ class numberHandler(tornado.web.RequestHandler):
         num = self.get_argument("num")
         if (num.isdigit()):
             r = "odd" if int(num) % 2 else "even"
-            self.write(num + "is" + r)
+            self.write(num+ " " + "is"+" " +r)
         else:
-            self.write(num + "is not a digit bae")
+            self.write(num + " " + "is not a digit bae")
+            
+class resourcesHandler(tornado.web.RequestHandler):
+    def get(self,studentName,courseId):
+        self.write("hello" + " " + studentName + " " + "you are currently viewing"+" " +courseId + " " + "in this page")
 
+class listHandler(tornado.web.RequestHandler):
+    def get(self):
+        f = open("list.txt", "r")
+        movie = f.read().splitlines()
+        f.close()
+        self.write(json.dumps(movie))
+    def post(self):
+        movie = self.get_argument("movie")
+        f = open("list.txt", "a")
+        f.write(movie)
+        f.write("\n")
+        f.close()
+        self.write(json.dumps("Its success da"))
+        
+        
 
 if __name__ == "__main__":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     app = tornado.web.Application([
         (r"/", basicRequestHandler),
         (r"/movielist", movieHandler),
-        (r"/numbercheck", numberHandler)
+        (r"/numbercheck", numberHandler),
+        (r"/list", listHandler),
+        (r"/students/([a-zA-Z]+)/([0-9]+)", resourcesHandler),
+        
     ])
 
     app.listen(2345)
